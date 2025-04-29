ee23ce2a6fdc2df3eaffc49faf5fdcf382a3cef4
FaKaWaTH <carbonell244@gmail.com>
2025-04-28 20:18:54 -0400

    rendering HTML

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::fs;
+
+async fn index() -> impl Responder {
+    match fs::read_to_string("templates/index.html") {
+        Ok(contents) => HttpResponse::Ok()
+            .content_type("text/html; charset=uft-8")
+            .body(contents),
+        Err(_) => HttpResponse::InternalServerError().body("Error al cargar el HTML"),
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(saludate))
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
-}
-
-#[get("/")]
-async fn saludate() -> impl Responder {
-    "Funciona?"
 }
