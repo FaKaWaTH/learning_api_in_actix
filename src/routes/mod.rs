db170a2f997f3f1ecbda084ee7b3f0464cbaf67d
FaKaWaTH <carbonell244@gmail.com>
2025-05-02 00:18:52 -0400

    add middleware layer

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,10 @@
 mod index;
 
-use axum::routing::{get, Router};
+use axum::{
+    middleware,
+    response::Response,
+    routing::{get, Router},
+};
 use index::index;
 
 use crate::web;
@@ -10,9 +14,17 @@ pub fn create_route() -> Router {
     Router::new()
         .merge(route_static())
         .merge(web::routes_login::route_api_login())
+        .layer(middleware::map_response(response_mapper))
 }
 
 //  stack static files in one fn
 fn route_static() -> Router {
     Router::new().route("/", get(index))
+}
+
+async fn response_mapper(res: Response) -> Response {
+    println!("->> {:<8} - response_mapper", "RES_MAPPER");
+
+    println!();
+    res
 }
