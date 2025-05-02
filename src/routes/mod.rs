50c995b26c050ed9db4f09f908a1ea4d2fbe1a1e
FaKaWaTH <carbonell244@gmail.com>
2025-05-01 23:30:50 -0400

    stack static files in one fn

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -7,6 +7,11 @@ use index::index;
 //  set post or get for each route
 pub fn create_route() -> axum::Router {
     axum::Router::new()
-        .route("/", axum::routing::get(index))
+        .merge(route_static())
         .route("/api", axum::routing::post(post_api))
+}
+
+//  stack static files in one fn
+fn route_static() -> axum::Router {
+    axum::Router::new().route("/", axum::routing::get(index))
 }
